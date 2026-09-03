835611ee9966a6b4840e3103feb039db78d374f3
badjoke-lab <badjoke.lab@gmail.com>
2026-09-03 21:36:11 +0900

    Promote Banei 2026-09-06 to reviewed A+ (#772)
    * feat: expose reviewed Banei public supplement
    
    * fix: publish reviewed Banei 2026-09-06 A+ detail

---
+++ src/lib/timetable/baneiReviewedSupplement.ts
@@ -0,0 +1,6 @@
+import baneiReviewedData from '../../../data/static/banei-2026-09-06-public-detail-v1.json';
+
+export const baneiReviewedMeetingId = 'banei-obihiro-racecourse-2026-09-06';
+
+export const baneiReviewedMeetingRow = baneiReviewedData.meeting;
+export const baneiReviewedMeetingDetail = baneiReviewedData.detail;
